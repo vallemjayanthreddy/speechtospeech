5ca259aabd9beb78a069a4ad4ef656931d0b1c7c
Jay <51223302+vallemjayanthreddy@users.noreply.github.com>
2024-06-03 16:05:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import openai
 import azure.cognitiveservices.speech as speechsdk
 deployment_name = "test-gpt-jay"
 openai.api_type = "azure"
-openai.api_key ="d65ae29b824e4b9e84000f40150fae59"
+openai.api_key ="Azureopenaikey"
 openai.api_base = "https://test-azureopenai-jay.openai.azure.com/"
 openai.api_version = "2024-02-01"
-AZURE_SPEECH_KEY="412a1362f5d742dc94d224092b1914e2"
+AZURE_SPEECH_KEY="AzureSpeechkey"
 AZURE_SPEECH_REGION="eastus"
 speechconfig = speechsdk.SpeechConfig(subscription=AZURE_SPEECH_KEY,region=AZURE_SPEECH_REGION)
 speechconfig.speech_recognition_language="en-US"
